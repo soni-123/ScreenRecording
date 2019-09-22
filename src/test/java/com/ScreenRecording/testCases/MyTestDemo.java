421384751fb2f20d6a0e294d8900670e8d4a32ad
soni-123 <rohitsoni912266@gmail.com>
2019-09-22 10:26:29 +0530

    First Commit

---
+++ src/test/java/com/ScreenRecording/testCases/MyTestDemo.java
@@ -0,0 +1,69 @@
+package com.ScreenRecording.testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import ScreenRecording.MyScreenRecorder;
+
+public class MyTestDemo {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup(){
+		System.setProperty("webdriver.chrome.driver","E:/Software/Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void navigationTest() throws Exception{
+		MyScreenRecorder.startRecording("navigationTest");
+		
+		driver.get("http://www.google.com");
+		driver.navigate().to("http://www.facebook.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		
+		MyScreenRecorder.stopRecording();
+	}
+	
+	@Test
+	public void navigation_FeatureTest() throws Exception{
+		MyScreenRecorder.startRecording("navigation_FeatureTest");
+		
+		driver.get("http://www.amazon.com");
+		driver.navigate().to("http://www.rediff.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		
+		MyScreenRecorder.stopRecording();
+	}
+	
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+
+}
